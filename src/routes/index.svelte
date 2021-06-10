f1a6856e75cfa27a9dbbbb6e32c2ed822af4965f
Lars Francke <lars.francke@gmail.com>
2021-06-10 14:13:12 +0200

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<script lang="ts">
+  import {Foo} from '$lib/test';
+
+  let a: Foo = {bar: "bla"};
+  console.log(a);
+</script>
